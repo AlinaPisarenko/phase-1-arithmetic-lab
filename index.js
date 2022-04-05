25afc94cbe3f5a2c8d669131fcfcdf0b6fedaea6
Alina <pisarenko.alina@gmail.com>
2022-04-04 20:30:12 -0400

    commit tree 5eac0ddcdf698b0a8fe70032c7f5657869d95519

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10 + 1);
+const max = Math.max(2, 5, 20, 14);
+const num3 = 12;
+const num4 = 16;
+const mod = num4 - num3;
